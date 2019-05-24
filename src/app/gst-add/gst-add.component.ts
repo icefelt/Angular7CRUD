799ae6192855555bad3718a5fc84bcbe6fbe4bcd
Scott Eissfeldt <scott@eissfeldt.com>
2019-05-23 22:04:15 -0700

    submit data to the node server

--- src/app/gst-add/gst-add.component.ts
+++ src/app/gst-add/gst-add.component.ts
@@ -2,6 +2,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+import { BusinessService } from '../business.service';
 
 @Component({
   selector: 'app-gst-add',
@@ -11,7 +12,7 @@ import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
 export class GstAddComponent implements OnInit {
 
   angForm: FormGroup;
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private bs: BusinessService) {
     this.createForm();
   }
 
@@ -21,6 +22,10 @@ export class GstAddComponent implements OnInit {
       business_name: ['', Validators.required ],
       business_gst_number: ['', Validators.required ]
     });
+  }
+
+  addBusiness(person_name, busines_name, business_gst_number) {
+    this.bs.addBusiness(person_name, busines_name, business_gst_number);
   }
 
   ngOnInit() {
