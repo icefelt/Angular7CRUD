caa730d82363f1162b265aeec63c553abea3cad1
Scott Eissfeldt <scott@eissfeldt.com>
2019-05-23 22:41:32 -0700

    create model and routes for application

--- api/server.js
+++ api/server.js
@@ -7,17 +7,19 @@ const express = require('express'),
     mongoose = require('mongoose'),
     config = require('./DB');
 
-    mongoose.Promise = global.Promise;
-    mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-      () => {console.log('Database is connected') },
-      err => { console.log('Can not connect to the database'+ err)}
-    );
+const businessRoute = require('./routes/business.route');
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => {console.log('Database is connected') },
+  err => { console.log('Can not connect to the database'+ err)}
+);
 
-    const app = express();
-    app.use(bodyParser.json());
-    app.use(cors());
-    const port = process.env.PORT || 4000;
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/business', businessRoute);
+const port = process.env.PORT || 4000;
 
-    const server = app.listen(port, function(){
-     console.log('Listening on port ' + port);
-    });
+const server = app.listen(port, function(){
+  console.log('Listening on port ' + port);
+});
